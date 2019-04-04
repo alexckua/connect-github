05cb340d065ab3906ddf86dad20a49456cab31d4
Alex Andreiev <alex@net.ck.ua>
2019-04-04 21:41:21 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,40 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '2.5.3'
+
+gem 'bootsnap', '>= 1.1.0', require: false
+gem 'bootstrap', '~> 4.3.1'
+gem 'coffee-rails', '~> 4.2'
+gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails'
+gem 'octokit', '~> 4.0'
+gem 'pg', '>= 0.18', '< 2.0'
+gem 'puma', '~> 3.11'
+gem 'rails', '~> 5.2.3'
+gem 'sass-rails', '~> 5.0'
+gem 'slim-rails'
+gem 'turbolinks', '~> 5'
+gem 'uglifier', '>= 1.3.0'
+
+group :development, :test do
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'pry-rails'
+  gem 'rspec-rails'
+  gem 'rubocop-performance'
+end
+
+group :development do
+  gem 'chromedriver-helper'
+  gem 'database_cleaner'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'rails-controller-testing'
+  gem 'rubocop-rspec'
+  gem 'selenium-webdriver'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'simplecov', require: false
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'web-console', '>= 3.3.0'
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'database_cleaner'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'simplecov'
+require 'faker'
+
+SimpleCov.start
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+end
