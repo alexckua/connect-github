971a5084d451af8bf2c823007e558080b392ab31
Alex Andreiev <alex@net.ck.ua>
2019-04-08 16:02:49 +0300

    fix flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,39 @@
 class SessionsController < ApplicationController
+  respond_to :js
+
+  layout false
+
   def create
     @user = User.find_by(nickname: user_params[:nickname], git_token: user_params[:git_token])
-    @user ||= User.create(user_params)
-    session[:user_id] = @user.id if @user.valid?
+    @user ||= User.new(user_params)
+    @git_user ||= GithubService.new(user_params[:git_token]).user
 
-    respond_to do |format|
-      format.js { render 'create', layout: false }
+    if @user.valid? && git_credentials_valid?
+      @user.save
+      session[:user_id] = @user.id
+      respond_with(flash.now[:success] = ['User was successfully login.'])
+    else
+      respond_with(flash.now[:error] = @user.errors.full_messages)
     end
   end
 
   def destroy
     session[:user_id] = nil
-
-    respond_to do |format|
-      format.js { render 'destroy', layout: false }
-    end
+    respond_with(flash.now[:notice] = ['User was successfully signout.'])
   end
 
   private
 
   def user_params
     params.require(:user).permit(:nickname, :git_token)
+  end
+
+  def git_credentials_valid?
+    if @git_user.exclude? '401'
+      true
+    else
+      @user.errors.messages[:'Git error'] = [@git_user]
+      false
+    end
   end
 end
